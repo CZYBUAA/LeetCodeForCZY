94071afc76044ea49dc7e4364ce5897e8448f270
CZYBUAA <644940329@qq.com>
2018-09-14 19:43:02 +0800

    判断是否为回文数

--- LeetCodeForCZY/src/test/Test.java
+++ LeetCodeForCZY/src/test/Test.java
@@ -1,19 +1,14 @@
 package test;
 
-import solution.TwoSum;;
+import solution.IsPalindrome;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		int[] nums = {3,-3};
-		int target = 0;
-		TwoSum twoSumtest=new TwoSum();
-		int[] result=twoSumtest.twoSum(nums, target);
-		System.out.println(result);
-		for(int i:result) {
-			System.out.println(i);
-		}
+		IsPalindrome ispalindromeTest=new IsPalindrome();
+		boolean i=ispalindromeTest.isPalindrome(11);
+		System.out.println(i);
 	}
 
 }
