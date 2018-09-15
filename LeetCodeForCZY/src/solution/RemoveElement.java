a6d29c1e9ae9e5718eb8a495df37d7d6f74b2fb1
CZYBUAA <644940329@qq.com>
2018-09-15 14:41:56 +0800

    新题：原地移除指定元素，无序

---
+++ LeetCodeForCZY/src/solution/RemoveElement.java
@@ -0,0 +1,24 @@
+package solution;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+        int result=nums.length;
+        
+        for(int i=0;i<result;i++) {				//注意这里不能用i<nums.length作为判定条件，因为数组有效长度是一直在变化的
+        	if(nums[i]==val) {
+        		while( nums[result-1]==val) {
+        			result--;
+        		}
+        		if(i<result) {
+        			nums[i]=nums[result-1];
+            		result--;
+        		}
+        	}
+        }
+        return result;
+    }
+}
+
+/*
+ * 在输入为[1],1时出现错误
+ */
--- LeetCodeForCZY/src/test/Test.java
+++ LeetCodeForCZY/src/test/Test.java
@@ -1,17 +1,16 @@
 package test;
 
-import solution.RemoveDuplicates;
+import solution.RemoveElement;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		RemoveDuplicates rd=new RemoveDuplicates();
-		int[] nums= {0,1,2,2,2,2,2,2,2,2,3,3,4,5,6};
-		int len = rd.removeDuplicates(nums);
+		RemoveElement re=new RemoveElement();
+		int[] nums= {2,0,1,2,4,2,2};
+		int vals=2;
+		int len = re.removeElement(nums, vals);
 
-		// 在函数里修改输入数组对于调用者是可见的。
-		// 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
 		for (int i = 0; i < len; i++) {
 		    System.out.print(nums[i]);
 		}	
