d29466831ed644f8dbd4639d3e63a11e40b68d29
CZYBUAA <644940329@qq.com>
2018-09-15 15:43:52 +0800

    主字符串中找子字符串，已解决

--- LeetCodeForCZY/src/solution/StrStr.java
+++ LeetCodeForCZY/src/solution/StrStr.java
@@ -2,29 +2,36 @@ package solution;
 
 public class StrStr {
 	public int strStr(String haystack, String needle) {
+		//处理主字符串长度小于子字符串的情况
 		if(haystack.length()<needle.length()) {
 			return -1;
 		}
+		//处理子字符串为空字符串或空时的情况
 		if(needle.length()==0||needle==null) {
 			return 0;
 		}
+		//处理主字符串和子字符串长度相等的情况
 		if(haystack.length()==needle.length()) {
 			if(haystack.equals(needle))
 				return 0;
 			else
 				return -1;
 		}
-		
+		//处理正常情况
 		for(int i=0;i<haystack.length();i++) {
 			int j=0;
 			int temp=i;
-			while(j<needle.length()) {
+			//要保证指针不越界，这是之前犯错的地方
+			while(j<needle.length()&&temp<haystack.length()) {
+				//如果对应位置任意一个字符不匹配，推出循环
 				if(!haystack.substring(temp, temp+1).equals(needle.substring(j,j+1))) {
 					break;
 				}
+				//若匹配，检查下一个字符，进入下一轮循环
 				j++;
 				temp++;
 			}
+			//若j的值与子字符串的长度相等，说明子字符串被完全遍历，说明已经找到匹配的子字符串
 			if(j==needle.length()) {
 				return i;
 			}
--- LeetCodeForCZY/src/test/Test.java
+++ LeetCodeForCZY/src/test/Test.java
@@ -7,8 +7,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		StrStr ss=new StrStr();
-		String haystack="mississippi";
-		String needle="issipi";
+		String haystack="0";
+		String needle="1";
 		int result=ss.strStr(haystack, needle);
 		System.out.print(result);	
 	}
