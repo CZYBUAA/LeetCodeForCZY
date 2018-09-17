7b696b8a7dd502c597bb2cf411feda5ba8f9586c
CZYBUAA <644940329@qq.com>
2018-09-17 15:57:13 +0800

    求开放，牛顿迭代法。原理上稍有问题

--- LeetCodeForCZY/src/solution/MySqrt.java
+++ LeetCodeForCZY/src/solution/MySqrt.java
@@ -2,15 +2,20 @@ package solution;
 
 public class MySqrt {
 	public int mySqrt(int x) {
-        int i=0;
-		while(i*i<=x) {
-			if(i*i==x) {
-				return i;
-			}
-			i++;
-		}
-		return i-1;
+		//牛顿迭代法!!!!
+		if (x < 0)
+            return -1;
+		if (x == 0)
+            return 0;
+        long r = x;
+        //当r^2>x的时候不断迭代，r^2=x的时候迭代结束，求得最终的解（按理说，r的最小值是x的开方，但是由于程序中有截断小数的情况，因此会出现r^2<x的情况）
+        //这里有个疑问，终止条件不应该是以误差控制为判断依据吗？
+        while (r * r > x) {
+        	r = (r + x / r) / 2;
+            System.out.println(r);
+        } 
+        return (int) r;
     }
 	
-	//输入为2147483647时超出运行时间限制
+	
 }
--- LeetCodeForCZY/src/test/Test.java
+++ LeetCodeForCZY/src/test/Test.java
@@ -7,9 +7,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		MySqrt ms=new MySqrt();
-		int result=ms.mySqrt(21);
+		int result=ms.mySqrt(2147483647);
 		System.out.println(result);
-
+		
 	}
 
 }
