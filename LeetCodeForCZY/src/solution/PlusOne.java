8ec0c7bd843633f6a7d80e1dda5806183ba1f1be
CZYBUAA <644940329@qq.com>
2018-09-17 14:06:16 +0800

    增加了优秀解法，删改了引起不必要循环的代码

--- LeetCodeForCZY/src/solution/PlusOne.java
+++ LeetCodeForCZY/src/solution/PlusOne.java
@@ -4,7 +4,25 @@ package solution;
 //时间太慢，解答中的典型案例就很好，还可以做修改
 public class PlusOne {
 	public int[] plusOne(int[] digits) {
-        int[] result=null;
+		//更好的解法
+		/*
+		int len = digits.length;
+
+        for (int j = len - 1; j >= 0; j--) {
+            if (digits[j] != 9) {
+                digits[j]++;
+                return digits;
+            }
+            digits[j] = 0;
+        }
+
+        int[] newDigits = new int[len + 1];
+        newDigits[0] = 1;
+        return newDigits;
+		*/
+        
+        //更改过后的解法
+		int[] result=null;
         boolean plusFlag=false;//判断是否向高位进一
         boolean currentPlusFlag=false;//判断当前位是否加一
         
@@ -12,45 +30,37 @@ public class PlusOne {
         	digits[digits.length-1]++;
         	return digits;
         }else {
-        	plusFlag=true;
+        	plusFlag=true;		//若等于9则说明初始时需要向高位进1
         }
         
         
         for(int i=digits.length-1;i>=0;i--) {
+        	//进位判断交换，若低位向本位进1，则当前位加一控制符设置为真，进位符设置为假
         	if(plusFlag==true) {
         		currentPlusFlag=true;
         		plusFlag=false;
         	}
         	
-        	//判断各位是否会产生进位
+        	//计算各值
         	if(currentPlusFlag==true) {
             	if(digits[i]==9) {
             		plusFlag=true;
             		
             		digits[i]=0;
             		currentPlusFlag=false;
-            		//判断最高位情况
+            		//判断最高位情况，根据需要增加数组元素
             		if(i==0) {
             			result=new int[digits.length+1];
             			result[0]=1;
-            			for(int j=1;j<digits.length+1;j++) {
-            				result[j]=digits[j-1];
-            				return result;
-            			}
+            			return result;
             		}
             	}else {
             		digits[i]++;
-            		plusFlag=false;
-            		currentPlusFlag=false;
+            		return digits;
             	}
             }else {
             	continue;
-            }
-        	//按照进位判断情况，计算各位的运算结果
-        	if(digits[i]==9 && plusFlag==true) {
-        		
-        	}
-        	
+            }	
         }
         return digits;
     }
--- LeetCodeForCZY/src/test/Test.java
+++ LeetCodeForCZY/src/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		PlusOne po=new PlusOne();
-		int[] nums= {8,9,9};
+		int[] nums= {9,9,9};
 		int[] result=po.plusOne(nums);
 		
 		for(int i:result) {
