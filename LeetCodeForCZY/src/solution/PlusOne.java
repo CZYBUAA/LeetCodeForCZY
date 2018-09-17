2ed661639c1943ea8dfc0db3321585c84da0476f
CZYBUAA <644940329@qq.com>
2018-09-17 13:54:41 +0800

    已解决上一版解答遗留的问题

--- LeetCodeForCZY/src/solution/PlusOne.java
+++ LeetCodeForCZY/src/solution/PlusOne.java
@@ -1,29 +1,57 @@
 package solution;
 
-//这个方法是有问题的，当数组长度够大时，一个long都放不了数组组合表达的数字
+
+//时间太慢，解答中的典型案例就很好，还可以做修改
 public class PlusOne {
 	public int[] plusOne(int[] digits) {
-
-        long sum=0;
-        for(int i=0,j=digits.length-1;i<digits.length;i++,j--) {
-        	sum+=digits[i]*Math.pow(10, j);        	
+        int[] result=null;
+        boolean plusFlag=false;//判断是否向高位进一
+        boolean currentPlusFlag=false;//判断当前位是否加一
+        
+        if(digits[digits.length-1]!=9) {
+        	digits[digits.length-1]++;
+        	return digits;
+        }else {
+        	plusFlag=true;
         }
         
-        sum+=1;
-        long temp=1;//除数
-        int digit=1;
-        while(!(sum/temp>=0&&sum/temp<=9)) {
-        	digit++;
-        	temp*=10;
-        }
         
-        char[] resultChar=new char[digit];
-        int[] result=new int[digit];
-        Long.toString(sum).getChars(0, digit, resultChar, 0);
-        for(int i=0;i<digit;i++) {
-        	result[i]=Integer.valueOf(Character.toString(resultChar[i]));
+        for(int i=digits.length-1;i>=0;i--) {
+        	if(plusFlag==true) {
+        		currentPlusFlag=true;
+        		plusFlag=false;
+        	}
+        	
+        	//判断各位是否会产生进位
+        	if(currentPlusFlag==true) {
+            	if(digits[i]==9) {
+            		plusFlag=true;
+            		
+            		digits[i]=0;
+            		currentPlusFlag=false;
+            		//判断最高位情况
+            		if(i==0) {
+            			result=new int[digits.length+1];
+            			result[0]=1;
+            			for(int j=1;j<digits.length+1;j++) {
+            				result[j]=digits[j-1];
+            				return result;
+            			}
+            		}
+            	}else {
+            		digits[i]++;
+            		plusFlag=false;
+            		currentPlusFlag=false;
+            	}
+            }else {
+            	continue;
+            }
+        	//按照进位判断情况，计算各位的运算结果
+        	if(digits[i]==9 && plusFlag==true) {
+        		
+        	}
+        	
         }
-        
-        return result;
+        return digits;
     }
 }
